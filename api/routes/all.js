62bbe635f99db1b77ab5e4583bc1085580f058c6
Pasha <vdonsk@gmail.com>
2022-10-25 19:37:38 +0300

    Commit and deploy
    [need deploy server]
    [need build client]

--- api/routes/all.js
+++ api/routes/all.js
@@ -3,7 +3,7 @@ const router = express.Router();
 
 router.all("*", (req, res, next) => {
   let domain =
-    process.env.NODE_ENV === "development"
+    process.env.NODE_ENV.trim() === "development"
       ? process.env.DEV_HOST
       : process.env.PROD_HOST;
 
