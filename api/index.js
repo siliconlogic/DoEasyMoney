53c04db26203337c5e9df7707979714ed8b48732
Pasha <vdonsk@gmail.com>
2022-10-26 22:33:23 +0300

    Commit and deploy
    [need deploy server]
    [need build client]

--- api/index.js
+++ api/index.js
@@ -1,12 +1,11 @@
-const express = require('express')
-const app = express()
-const port = 9000
+const express = require("express");
+const app = express();
+const port = 9001;
 
-app.get('/', (req, res) => {
-    res.send('DO easy money!')
-})
+app.get("/", (req, res) => {
+  res.send("DO easy money!");
+});
 
 app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`)
-})
-
+  console.log(`Example app listening on port ${port}`);
+});
